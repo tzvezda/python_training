b5ee6a8983c404013d600fedee83eb77bf2149a0
tzvezda <galunenko@gmail.com>
2017-01-09 22:14:48 +0700

    исправлен тест модификации контактов

--- test/test_contact_modify.py
+++ test/test_contact_modify.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_modify_first_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="TEST1", middlename="TEST1", lastname="TEST1", nickname="TEST1", title="TEST1",
+    app.contact.modify_first_contact (Contact(firstname="TEST1", middlename="TEST1", lastname="TEST1", nickname="TEST1", title="TEST1",
                                company="TEST1", address="TEST1", home="TEST1", mobile="TEST1", work="TEST1", fax="TEST1",
                                email="TEST1", email2="TEST1", email3="TEST1", homepage="TEST1", bday="0",
                                bmonth="0", byear="1990", aday="0", amonth="0", ayear="1990", group="TEST1",
